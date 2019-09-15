6c64f4c1737e60b471ae6e94d8c85f9ad89c11a2
Michael B <michael@ramyun.co.uk>
2019-09-15 13:37:17 +0100

    Add env port to server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
